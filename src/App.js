5cf0f6c8bd535b691ab2ed6ae1d8518f04b0042c
Arttu Hartikainen <arttuhartikainen@Arttu-MacBook-Pro.local>
2021-08-20 21:24:11 +0300

    Heading component structure and styling
    Add background image to Heading component. Add heading text to Heading component. Styling for Heading component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './scss/App.scss';
 
+import Heading from './components/Heading';
+
+
 function App() {
   return (
     <div className="App">
-      
+      <Heading />
     </div>
   );
 }
---
+++ src/components/Heading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Heading() {
+    return (
+        <div className='headingContainer'>
+            <div className='headingText'>
+                <h1>Star wars films</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Heading
