de1704568acab2d447dad6c19101aa5a4aae392e
Arttu Hartikainen <arttuhartikainen@Arttu-MacBook-Pro.local>
2021-08-21 15:17:04 +0300

    Small fixes and clean up
    Small fixes and clean up for Heading and Pagination components

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Heading() {
-    return (
-        <div className='headingContainer'>
-            <div className='headingText'>
-                <h1>Star wars films</h1>
-            </div>
-        </div>
-    )
+	return (
+		<div className="heading">
+			<div className="headingText">
+				<h1>Star wars films</h1>
+			</div>
+		</div>
+	);
 }
 
-export default Heading
+export default Heading;
