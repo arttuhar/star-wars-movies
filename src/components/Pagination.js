1490bf91feea756299d5cf31b9691a9ad12a9a03
Arttu Hartikainen <arttuhartikainen@Arttu-MacBook-Pro.local>
2021-08-23 17:47:19 +0300

    Add arrow icons to pagination buttons, style pagination buttons
    Add FontAwesome arrow icons to pagination buttons. Style pagination buttons.

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,12 +1,29 @@
 import React from "react";
 import ReactPaginate from "react-paginate";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
+
 function Pagination(props) {
 	return (
 		<div className="pagination">
 			<ReactPaginate
-				previousLabel={"Previous page"}
-				nextLabel={"Next page"}
+				previousLabel={
+					<div className="prevButtonSpan">
+						<span className="prevButtonIcon">
+							<FontAwesomeIcon icon={faCaretLeft} />
+						</span>
+						<span className="prevButtonText">Previous page</span>
+					</div>
+				}
+				nextLabel={
+					<span className="nextButtonSpan">
+						<span className="nextButtonText">Next page</span>
+						<span className="nextButtonIcon">
+							<FontAwesomeIcon icon={faCaretRight} />
+						</span>
+					</span>
+				}
 				pageCount={props.pageCount}
 				onPageChange={props.pageChange}
 				pageRangeDisplayed={0}
