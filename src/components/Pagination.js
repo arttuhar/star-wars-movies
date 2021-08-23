f1d1e72b7b77327c7cef6dc1dd9f745e3118da1a
Arttu Hartikainen <arttuhartikainen@Arttu-MacBook-Pro.local>
2021-08-23 10:37:48 +0300

    Add media queries to Container and GridItem components
    Add media queries to Container and GridItem component to show results in responsive layout.

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,30 +1,27 @@
-import React from 'react'
-import ReactPaginate from 'react-paginate';
+import React from "react";
+import ReactPaginate from "react-paginate";
 
 function Pagination(props) {
-
-
-    return (
-        <div className='pagination'>
-            <ReactPaginate
-                previousLabel={'Previous page'}
-                nextLabel={'Next page'}
-                pageCount={props.pageCount}
-                onPageChange={props.pageChange}
-                pageRangeDisplayed={0}
-                marginPagesDisplayed={0}
-                breakLabel={''}
-
-                containerClassName={'paginationContainer'}
-                previousLinkClassName={'paginationPrevious'}
-                previousClassName={'previous'}
-                nextLinkClassName={'paginationNext'}
-                nextClassName={'next'}
-                disabledClassName={'paginationDisabled'}
-                activeClassName={'paginationActive'}
-            />
-        </div>
-    )
+	return (
+		<div className="pagination">
+			<ReactPaginate
+				previousLabel={"Previous page"}
+				nextLabel={"Next page"}
+				pageCount={props.pageCount}
+				onPageChange={props.pageChange}
+				pageRangeDisplayed={0}
+				marginPagesDisplayed={0}
+				breakLabel={""}
+				containerClassName={"paginationContainer"}
+				previousLinkClassName={"paginationPrevious"}
+				previousClassName={"previous"}
+				nextLinkClassName={"paginationNext"}
+				nextClassName={"next"}
+				disabledClassName={"paginationDisabled"}
+				activeClassName={"paginationActive"}
+			/>
+		</div>
+	);
 }
 
-export default Pagination
+export default Pagination;
